a5f85ed8fbf1948fd8e115a771b9aed04b5701c0
Chretien <jeanchretienhabu@gmail.com>
2025-04-04 19:39:16 +0200

    i added postlist

--- react-git/README.md
+++ react-git/README.md
@@ -1,1 +1,3 @@
 i am currently learning git and git hub, how to use it
+## sub header 
+watch my learning
---
+++ react-git/src/postlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PostList(){
+
+    return(
+    <div>
+<h3>post lists goes here</h3>
+    </div>)
+}
